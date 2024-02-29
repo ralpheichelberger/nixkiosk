0b797e9e2b0853b9cab56f14fdea3a61f45fcea3
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2024-02-29 17:53:23 +0100

    infoboard

---
+++ configuration_infoboard.nix
@@ -0,0 +1,38 @@
+{ modulesPath, config, lib, pkgs, ... }: {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./disk-config.nix
+  ];
+  boot.loader.grub = {
+    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+    # devices = [ ];
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
+  services.openssh.enable = true;
+
+  services.cage = {
+    enable = true;
+    user = "signage";
+    program = (pkgs.chromium).outPath+"/bin/chromium --kiosk http://mula-mfs5/page111.php";
+  };
+   users.users.signage = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+    ];
+  };
+
+
+  environment.systemPackages = map lib.lowPrio [
+    pkgs.curl
+    pkgs.gitMinimal
+  ];
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    (builtins.readFile ./id_rsa.pub)
+  ];
+
+  system.stateVersion = "23.11";
+}
--- flake.nix
+++ flake.nix
@@ -5,29 +5,14 @@
 
   outputs = { nixpkgs, disko, ... }:
     {
-      nixosConfigurations.hetzner-cloud = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.infoboard = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           disko.nixosModules.disko
-          ./configuration.nix
+          ./configuration2.nix
         ];
       };
-      # tested with 2GB/2CPU droplet, 1GB droplets do not have enough RAM for kexec
-      nixosConfigurations.digitalocean = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          { disko.devices.disk.disk1.device = "/dev/vda"; }
-          ./configuration.nix
-        ];
-      };
-      nixosConfigurations.hetzner-cloud-aarch64 = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
-        modules = [
-          disko.nixosModules.disko
-          ./configuration.nix
-        ];
-      };
+      
     };
     
 }
